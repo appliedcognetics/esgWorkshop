27651a840e368afaea6d655c4676388fd6c17d47
Chris Coulthrust <coulthrust@gmail.com>
2023-02-15 15:22:23 -0500

    save bash script

--- src/workshop/databasescripts/db_bashscript.sh
+++ src/workshop/databasescripts/db_bashscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for n in ~/clouddrive/'01 - Tables'/*sql; do sqlcmd -i "$n"; done
-for n in ~/clouddrive/'02 - User Defined Functions'/*.sql; do sqlcmd -i "$n"; done
-for n in ~/clouddrive/'04 - Stored Procedures'/*.sql; do sqlcmd -i "$n"; done
+for n in ./'01 - Tables'/*sql; do sqlcmd -i "$n"; done
+for n in ./'02 - User Defined Functions'/*.sql; do sqlcmd -i "$n"; done
+for n in ./'04 - Stored Procedures'/*.sql; do sqlcmd -i "$n"; done
